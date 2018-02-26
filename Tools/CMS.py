5f044bef4a55900de1cc97ca6118e576df65353d
Mfr12 <31842416+Mfr12@users.noreply.github.com>
2018-02-26 19:06:36 +0330

    DPredator

--- Tools/CMS.py
+++ Tools/CMS.py
@@ -5,7 +5,7 @@ import os
 if os.path.isdir("Tools/CMSmap") == True:
     pass
 else:
-    os.system("git clone https://github.com/Dionach/CMSmap.git")
+    os.system("cd Tools && git clone https://github.com/Dionach/CMSmap.git")
 host = input("{CMS Map}\n\nEnter Your Host: ")
 os.system("cd Tools/CMSmap && python cmsmap.py -t " + host)
 wait = input("Press [Enter] to go back...")
