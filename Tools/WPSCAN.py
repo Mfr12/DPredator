d0b816039794ecb6d6af2974a223ff549ad1b6aa
Mfr12 <31842416+Mfr12@users.noreply.github.com>
2018-02-26 19:09:42 +0330

    DPredator

--- Tools/WPSCAN.py
+++ Tools/WPSCAN.py
@@ -2,9 +2,9 @@
 
 import os
 
-check = os.path.isdir("wpscan")
+check = os.path.isdir("Tools/wpscan")
 if not check:
-    os.system("git clone https://github.com/wpscanteam/wpscan.git")
+    os.system("cd Tools && git clone https://github.com/wpscanteam/wpscan.git")
 os.system("clear")
 host = input("Enter your Host (or either...): ")
 host = "--no-banner --random-agent --url " + host
@@ -13,11 +13,11 @@ os.system("clear")
 print("{WP Scan}\n{1}User Enumeration\n{2}Plugin Enumeration\n{3}Enumerate All\n{0}Main Menu")
 a = int(input())
 if a == 1:
-    os.system("cd wpscan && ruby wpscan.rb " + host + " --enumerate u")
+    os.system("cd Tools/wpscan && ruby wpscan.rb " + host + " --enumerate u")
 elif a == 2:
-    os.system("ruby wpscan/wpscan.rb " + host + " --enumerate p")
+    os.system("ruby Tools/wpscan/wpscan.rb " + host + " --enumerate p")
 elif a == 3:
-    os.system("ruby wpscan/wpscan.rb " + host + " --enumerate")
+    os.system("ruby Tools/wpscan/wpscan.rb " + host + " --enumerate")
 elif a == 0:
     os.system("python3 DPredator.py")
 os.system("python3 DPredator.py")
