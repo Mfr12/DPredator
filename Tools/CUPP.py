cb51baa223f3f6b31f5491dd6de9b05383822cd2
Mfr12 <31842416+Mfr12@users.noreply.github.com>
2018-02-27 17:31:54 +0330

    DPredator

--- Tools/CUPP.py
+++ Tools/CUPP.py
@@ -2,14 +2,17 @@
 import os
 from termcolor import colored
 
-if os.path.isdir("Tools/cupp") == True:
+path = open("path.txt","r")
+path = path[0]
+
+if os.path.isdir(path + "/Tools/cupp") == True:
     print("{Cupp}\n\nMake dictionary file for your victim:")
-    os.system("cd Tools/cupp && python cupp.py -i")
+    os.system("cd " + path + "/Tools/cupp && python cupp.py -i")
 else:
-    os.system("cd Tools && git clone https://github.com/Mebus/cupp.git")
-    os.system("cd Tools/cupp && python cupp.py -i")
+    os.system("cd " + path + "/Tools && git clone https://github.com/Mebus/cupp.git")
+    os.system("cd " + path + "/Tools/cupp && python cupp.py -i")
 
 print(colored("\n","white"))
-os.system("cd Tools/cupp && echo You can access your dictionary in: $PWD")
+os.system("cd " + path + "/Tools/cupp && echo You can access your dictionary in: $PWD")
 wait = input("Press [Enter] to go back...")
 os.system("python3 DPredator.py")
